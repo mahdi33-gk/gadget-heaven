4cb7064facf5ed9893054b556d3f0d46e1c09b2f
mehedi <mehedi25122003@gmail.com>
2025-03-28 19:00:50 +0600

    dashboard section start

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const Dashboard = () => {
-    return (
+  return (
+    <div className="w-10/12 mx-auto">
+      <div>
         <div>
-            <h1>this is dashboard!</h1>
+          <h1>Dashboard</h1>
+          <p>
+            Explore the latest gadgets that will take your experience to the
+            next level. From smart devices to the coolest accessories, we have
+            it all!
+          </p>
         </div>
-    );
+        <Tabs>
+          <TabList>
+            <Tab>Title 1</Tab>
+            <Tab>Title 2</Tab>
+          </TabList>
+
+          <TabPanel>
+            <h2>Any content 1</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;

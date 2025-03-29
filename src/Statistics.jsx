17f5eb7ca4f24cc5e75f65c485a9cd4b6eee7da2
mehedi <mehedi25122003@gmail.com>
2025-03-29 10:32:41 +0600

    bar not fixed

--- src/Statistics.jsx
+++ src/Statistics.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import { BarChart } from "@mui/x-charts/BarChart";
 
 const Statistics = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <BarChart
+        xAxis={[
+          {
+            id: "barCategories",
+            data: ["bar A", "bar B", "bar C"],
+            scaleType: "band",
+          },
+        ]}
+        series={[
+          {
+            data: [2, 5, 3],
+          },
+        ]}
+        width={500}
+        height={300}
+      />
+    </div>
+  );
 };
 
 export default Statistics;
--- src/main.jsx
+++ src/main.jsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
         element: <Dashboard></Dashboard>
       },
       {
-        path:'/statisitcs',
+        path:'statisitcs',
         element: <Statistics></Statistics>
       }
     ]
